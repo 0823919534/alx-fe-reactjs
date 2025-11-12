bee2da2639b46a81c86c44a1567478bab777d0fe
TB Mokgadi <bevmoja@gmail.com>
2025-11-12 21:16:26 +0200

    feat(store): add recipe store using Zustand (recipes, addRecipe)

--- src/stores/recipeStore.js
+++ src/stores/recipeStore.js
@@ -1,7 +1,7 @@
-import create from 'zustand';
+import { create } from 'zustand';  // notice { create }
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
 }));
