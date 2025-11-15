ffed487fcb3f47b8420b0245baba0c82a24a3ae7
TB Mokgadi <bevmoja@gmail.com>
2025-11-15 09:32:15 +0200

    Implement AddRecipeForm component

--- recipe-sharing-app/src/AddRecipeForm.jsx
+++ recipe-sharing-app/src/AddRecipeForm.jsx
@@ -1,29 +1,5 @@
-import React, { useState } from 'react';
-import { useRecipeStore } from './store';
+import React from 'react';
 
-function AddRecipeForm() {
-  const [name, setName] = useState('');
-  const addRecipe = useRecipeStore((state) => state.addRecipe);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (name.trim()) {
-      addRecipe({ name });
-      setName('');
-    }
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type='text'
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder='Recipe name'
-      />
-      <button type='submit'>Add Recipe</button>
-    </form>
-  );
+export default function AddRecipeForm() {
+  return <div>Add Recipe Form</div>;
 }
-
-export default AddRecipeForm;
