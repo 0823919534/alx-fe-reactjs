01f02bf58f180464d98215083572fd5656718880
TB Mokgadi <bevmoja@gmail.com>
2025-11-15 09:15:40 +0200

    Implement RecipeList component

--- recipe-sharing-app/src/RecipeList.jsx
+++ recipe-sharing-app/src/RecipeList.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { useRecipeStore } from './store';
 
-export default function RecipeList() {
-    const recipes = useRecipeStore((state) => state.recipes);
-    return (
-        <div>
-            <h2>Recipe List</h2>
-            <ul>
-                {recipes.map((recipe, index) => (
-                    <li key={index}>{recipe}</li>
-                ))}
-            </ul>
-        </div>
-    );
+function RecipeList() {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <ul>
+      {recipes.map((recipe, index) => (
+        <li key={index}>{recipe.name}</li>
+      ))}
+    </ul>
+  );
 }
+
+export default RecipeList;
