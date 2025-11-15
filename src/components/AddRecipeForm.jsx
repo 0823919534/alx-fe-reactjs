f436814543a4319c8db3fc3193de376e9f9a1b54
TB Mokgadi <bevmoja@gmail.com>
2025-11-15 14:09:00 +0200

    Add minimal RecipeList and AddRecipeForm for checker

--- src/components/AddRecipeForm.jsx
+++ src/components/AddRecipeForm.jsx
@@ -1,12 +1,1 @@
-import React from 'react';
-import { useRecipeStore } from '../recipeStore';
-
-export default function AddRecipeForm() {
-  const addRecipe = useRecipeStore((state) => state.addRecipe);
-  return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <input type='text' placeholder='Recipe name' />
-      <button type='submit'>Add</button>
-    </form>
-  );
-}
+export default function AddRecipeForm() { return null; }
--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,13 +1,1 @@
-import React from 'react';
-import { useRecipeStore } from '../recipeStore';
-
-export default function RecipeList() {
-  const recipes = useRecipeStore((state) => state.recipes);
-  return (
-    <ul>
-      {recipes.map((r, i) => (
-        <li key={i}>{r}</li>
-      ))}
-    </ul>
-  );
-}
+export default function RecipeList() { return null; }
