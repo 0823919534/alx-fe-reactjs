76e4d4b6754619652018025b8a2704df66cde61a
TB Mokgadi <bevmoja@gmail.com>
2025-11-16 08:03:43 +0200

    Add RecipeList.jsx component

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,16 +1,20 @@
+import React from 'react';
 import { useRecipeStore } from '../recipeStore';
 
 export default function RecipeList() {
-  const recipes = useRecipeStore(state => state.recipes);
-
+  const recipes = useRecipeStore((state) => state.recipes || []);
   return (
     <div>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      {recipes.length === 0 ? (
+        <p>No recipes yet</p>
+      ) : (
+        recipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 }
