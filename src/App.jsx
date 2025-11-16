4c8f80009272f60a49a199413006002d3eb909ce
TB Mokgadi <bevmoja@gmail.com>
2025-11-16 10:54:30 +0200

    Integrate RecipeList and AddRecipeForm in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+@"
 import React from 'react';
-import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
 
 export default function App() {
   return (
@@ -11,3 +12,4 @@ export default function App() {
     </div>
   );
 }
+"@ | Set-Content -Path src\App.jsx -Encoding UTF8
