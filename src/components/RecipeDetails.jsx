f381ccca072cca47f56ac6c1308c4f893de9b5eb
TB Mokgadi <bevmoja@gmail.com>
2025-11-16 12:36:19 +0200

    Add RecipeDetails component showing recipe info

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,32 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useRecipeStore } from '../recipeStore';
 import EditRecipeForm from './EditRecipeForm';
 import DeleteRecipeButton from './DeleteRecipeButton';
 
-export default function RecipeDetails() {
+const RecipeDetails = () => {
   const { id } = useParams();
-  const recipeId = Number(id);
   const recipe = useRecipeStore((state) =>
-    state.recipes.find((r) => r.id === recipeId)
+    state.recipes.find((r) => r.id === Number(id))
   );
-
-  const [isEditing, setIsEditing] = useState(false);
 
   if (!recipe) return <p>Recipe not found</p>;
 
   return (
     <div>
-      {isEditing ? (
-        <EditRecipeForm recipe={recipe} onClose={() => setIsEditing(false)} />
-      ) : (
-        <>
-          <h1>{recipe.title}</h1>
-          <p>{recipe.description}</p>
-          <button onClick={() => setIsEditing(true)}>Edit Recipe</button>
-          <DeleteRecipeButton recipeId={recipe.id} />
-        </>
-      )}
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipe.id} />
     </div>
   );
-}
+};
+
+export default RecipeDetails;
