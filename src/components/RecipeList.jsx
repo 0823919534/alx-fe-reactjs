1b3d9b481e08b6c8c39169c9f2ace2ef28af772d
TB Mokgadi <bevmoja@gmail.com>
2025-11-16 12:22:22 +0200

    Add links to individual recipe details in RecipeList

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useRecipeStore } from '../recipeStore';
+import { Link } from 'react-router-dom';
 
 export default function RecipeList() {
-  const recipes = useRecipeStore((state) => state.recipes);
+  const recipes = useRecipeStore((state) => state.recipes || []);
 
   return (
     <div>
@@ -11,7 +12,9 @@ export default function RecipeList() {
       ) : (
         recipes.map((recipe) => (
           <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
+            <h3>
+              <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+            </h3>
             <p>{recipe.description}</p>
           </div>
         ))
