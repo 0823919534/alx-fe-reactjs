7dc2c9bce316d8762c7232c2a15e0ddf510cecfb
TB Mokgadi <bevmoja@gmail.com>
2025-11-16 12:33:30 +0200

    Enhance recipeStore with updateRecipe and deleteRecipe actions

--- src/recipeStore.js
+++ src/recipeStore.js
@@ -2,17 +2,22 @@ import create from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((r) =>
         r.id === updatedRecipe.id ? updatedRecipe : r
       ),
     })),
-  deleteRecipe: (recipeId) =>
+
+  deleteRecipe: (id) =>
     set((state) => ({
-      recipes: state.recipes.filter((r) => r.id !== recipeId),
+      recipes: state.recipes.filter((r) => r.id !== id),
     })),
+
   setRecipes: (recipes) => set({ recipes }),
 }));
 
