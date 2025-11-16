52328ba2067eb4839c019219dfd63135f1c32cd4
TB Mokgadi <bevmoja@gmail.com>
2025-11-16 10:00:23 +0200

    Implement recipeStore (Zustand)

--- src/recipeStore.js
+++ src/recipeStore.js
@@ -1,11 +1,7 @@
 import create from 'zustand';
 
-const useRecipeStore = create((set) => ({
+export const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
 }));
-
-// export both named and default to satisfy different checker expectations
-export { useRecipeStore };
-export default useRecipeStore;
