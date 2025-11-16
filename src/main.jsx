d422195dbf71166dc1023e1c4c0b6da6339af2f0
TB Mokgadi <bevmoja@gmail.com>
2025-11-16 09:08:52 +0200

    Add src/main.jsx entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const rootEl = document.getElementById('root');
+if (!rootEl) {
+  throw new Error('Missing <div id=\"root\"></div> in index.html');
+}
+createRoot(rootEl).render(<App />);
